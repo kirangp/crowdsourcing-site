e9915d1b974028935b9b51549e66ae80f0098f00
kirangp <kirangp@gmail.com>
2017-04-17 22:29:16 +1000

    Can edit projects and rewards

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,44 +1,63 @@
 class ProjectsController < ApplicationController
-    before_action :authenticate_user!, except: [:index, :show]
-    before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
-    def index
-        @projects = Project.all
-        @displayed_projects = Project.take(4)
+  def index
+    @projects = Project.all
+    @displayed_projects = Project.take(4)
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+  end
+
+  def create
+    @project = current_user.projects.build(project_params)
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to @project, notice: 'Project was successfully created' }
+        format.json { render :show, status: :ok, location: @project }
+      else
+        format.html { redirect_to :edit }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def show
+  def update
+    respond_to do |format|
+      if @project.update(project_params)
+        format.html { redirect_to @project, notice: 'Project was successfully updated' }
+        format.json { render :show, status: :ok, location: @project }
+      else
+        format.html { render :edit }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def new
-        @project = Project.new
+  def destroy
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to project_path, notice: "Project was successfully destroyed" }
+      format.json { head :no_content }
     end
+  end
 
-    def edit
-    end
+  def set_project
+    @project = Project.find(params[:id])
+  end
 
-    def create
-        @project = current_user.projects.build(project_params)
+  private
 
-        respond_to do |format|
-            if @project.save
-                format.html { redirect_to @project, notice: 'Project was successfully created' }
-                format.json { render :show, status: :ok, location: @project }
-            else
-                format.html { redirect_to :edit }
-                format.json { render json: @project.errors, status: :unprocessable_entity }
-            end
-        end
-    end
-
-
-    def set_project
-        @project = Project.find(params[:id])
-    end
-
-    private
-
-    def project_params
-        params.require(:project).permit(:name, :short_description, :description, :goal, :image_url, :expiration_date)
-    end
+  def project_params
+    params.require(:project).permit(:name, :short_description, :description, :goal, :image_url, :expiration_date)
+  end
   end
--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,32 +1,56 @@
 class RewardsController < ApplicationController
-    before_action :authenticate_user!, :set_project
+  before_action :authenticate_user!, :set_project
+  before_action :set_reward, except: [:new, :create]
 
-    def new
-        @reward = @project.rewards.build
-        respond_to do |format|
-            format.html
-        end
+  def new
+    @reward = @project.rewards.build
+    respond_to do |format|
+      format.html
     end
+  end
 
-    def create
-        @reward = @project.rewards.build(reward_params)
-        respond_to do |format|
-            if @reward.save
-                format.html { redirect_to @project, notice: "Reward was successfully created" }
-            else
-                format.html { render :new }
-            end
-        end
+  def create
+    @reward = @project.rewards.build(reward_params)
+    respond_to do |format|
+      if @reward.save
+        format.html { redirect_to @project, notice: 'Reward was successfully created' }
+      else
+        format.html { render :new }
+      end
     end
+  end
 
-
-    private
-
-    def set_project
-        @project = Project.find(params[:project_id])
+  def update
+    respond_to do |format|
+      if @reward.update(reward_params)
+        format.html { redirect_to @project, notice: 'Reward was successfully updated' }
+      else
+        format.html { render :edit }
+      end
     end
+  end
 
-    def reward_params
-        params.require(:reward).permit(:name, :description, :value, :shipping, :number_available, :estimated_delivery)
+  def edit
+  end
+
+  def destroy
+    @reward.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_path(@project), notice: 'Reward was successfully destroyed' }
     end
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def set_reward
+    @reward = @project.rewards.find(params[:id])
+  end
+
+  def reward_params
+    params.require(:reward).permit(:name, :description, :value, :shipping, :number_available, :estimated_delivery)
+  end
 end
